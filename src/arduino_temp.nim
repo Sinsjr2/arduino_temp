ca5910caac926c6ff3252362a0cf6f3220dab0b1
Sinsjr2 <hks15sirubia@gmail.com>
2020-09-24 18:35:47 +0900

    unix時間を西暦に変換する関数を追加しました。

--- src/arduino_temp.nim
+++ src/arduino_temp.nim
@@ -4,6 +4,14 @@ import sd/SD as s
 # import sd/utility/Sd2Card as c
 import strutils
 
+const GMT_TOKYO* : uint32 = 9*60*60
+const SECONDS_IN_A_DAY* =  24*60*60
+const EPOCH_DAY*          = (1969 * 365) + (1969 div 4) - (1969 div 100) + (1969 div 400) + 306 #  days from 0000/03/01 to 1970/01/01
+const UNIX_EPOCH_DAY*     = EPOCH_DAY
+const YEAR_ONE*  = 365
+const YEAR_FOUR* = YEAR_ONE * 4 + 1  #  it is YEAR_ONE*4+1 so the maximum reminder of day / YEAR_FOUR is YEAR_ONE * 4 and it occurs only on 2/29
+const YEAR_100*  = YEAR_FOUR * 25 - 1
+const YEAR_400*  = YEAR_100 * 4 + 1    #  it is YEAR_100*4+1 so the maximum reminder of day / YEAR_400 is YEAR_100 * 4 and it occurs only on 2/29
 
 #var rxbuf: string = ""
 #var newData: bool = false
@@ -38,6 +46,91 @@ import strutils
 #     return false
 #   Serial.println "SD initialized"
 #   return true
+
+type Time = object
+  ## 時間を表します。
+  seconds: int64
+  nanoseconds : int32
+
+type Date = object
+  nanoseconds*: int32
+  second*: int8
+
+  minute*: int8
+
+  hour*: int8
+
+  monthday*: int8
+  month*: int8
+  year*: int16
+
+func addMilliSec(this : Time, addMsec : uint32 ) : Time =
+  ## 現在の時間に指定されたミリ秒を追加します。
+  const nanoSecondMax = 999_999_999'u32
+  # オーバーフローしないように型を変更
+  # ナノ秒単位に変換
+  let converted = addMsec * 1000'u64
+  let ns = this.nanoseconds.uint64 + converted
+  # あふれることが無いので元の型に戻す
+  let currentNsec = (ns mod nanoSecondMax).int32
+
+  # 秒数に関してはオーバーフローする可能性があるが、それは現実的ではない大きな値なので無視する
+  let currentMsec = this.seconds + (ns div nanoSecondMax).int64
+
+  return Time(seconds : currentMsec, nanoseconds: currentNsec)
+
+proc getDate*(this : Time) : Date =
+  ## 引数のUnix時間を西暦表記に変更します。
+  ## 参考 http://mrkk.ciao.jp/memorandom/unixtime/unixtime.html
+
+  const monthday = [ 0'u16,31,61,92,122,153,184,214,245,275,306,337 ]
+
+  var unixtime = this.seconds + GMT_TOKYO.int64
+  let second = (unixtime mod 60).int8
+  let minute = ((unixtime div 60) mod 60).int8
+  let hour = ((unixtime div 3600) mod 24).int8
+
+  var unixday = (unixtime div SECONDS_IN_A_DAY).uint32
+  let weekday = ((unixday + 3) mod 7).uint8 #  because the unix epoch day is thursday
+  #  days from 0000/03/01 to 1970/01/01
+  unixday = unixday + UNIX_EPOCH_DAY
+  discard Serial.print(unixday, 10'u8)
+  Serial.println("")
+
+  var year = 400 * (unixday div YEAR_400)
+  unixday = unixday mod YEAR_400
+
+  var n = (unixday div YEAR_100).uint32;
+  year += (n * 100).uint16
+  unixday = unixday mod YEAR_100
+
+  var leap = false
+  if n == 4:
+      leap = true
+  else:
+      year += 4 * (unixday div YEAR_FOUR).uint16
+      unixday = unixday mod YEAR_FOUR
+      
+      n = (unixday div YEAR_ONE).uint16
+      year += n.uint16
+      unixday = unixday mod YEAR_ONE;
+      if n == 4:
+          leap = true
+
+  var month : int8
+  var day : int8
+  if leap :
+      month = 2;
+      day = 29;
+  else :
+      let monthIndex = ((unixday * 5 + 2) div 153).int8
+      day = (unixday - monthday[monthIndex] + 1).int8
+      month = (monthIndex + 3) mod 12
+      ## 翌年の1、2、3月の場合
+      if month <= 3 :
+          year = year + 1
+    
+  return Date(nanoseconds : this.nanoseconds, second : second, minute : minute, hour : hour, monthday : day, month : month, year : year.int16)
 
 proc readI2CTemplature() : float =
   ## i2c経由で温度センサの値を読み込みます。
